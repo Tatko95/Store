c971d686795fb257d330bb8abb8240ce8f27367d
Andrey Palamarchuk <studypalamarchuk@ukr.net>
2016-04-17 16:05:02 +0300

    Добавив та настроїв юніті

--- Store.Web/Controllers/HomeController.cs
+++ Store.Web/Controllers/HomeController.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Store.Model.Abstract.Service;
 using System.Web.Mvc;
 
 namespace Store.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IFirstService service;
+        public HomeController(IFirstService service)
+        {
+            this.service = service;
+        }
+
         public ActionResult Index()
         {
+            //var t = ((FirstService)service).Insert(new Model.Entities.dbml.DiliveryType() { Name = "FirstTest" });
             return View();
         }
     }
