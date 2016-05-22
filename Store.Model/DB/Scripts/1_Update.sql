351ad43c918a1038a505c2b4ae66040eb1854b46
Ivan Kornilov <studypalamarchuk@ukr.net>
2016-05-22 18:02:26 +0300

    Update DB scripts

--- Store.Model/DB/Scripts/1_Update.sql
+++ Store.Model/DB/Scripts/1_Update.sql
@@ -1,8 +1,9 @@
 ﻿USE StoreDB
 
 ALTER TABLE [Product]
-ADD ProductCategoryId int foreign key references [ProductCategory](Id)
+ADD ProductCategoryId int,
+constraint FK_prod_ProdCatId_prodCat_Id foreign key (ProductCategoryId) references [ProductCategory](Id)
 
-ALTER TABLE [ProductDetails] drop constraint FK__ProductDe__Produ__1920BF5C
+ALTER TABLE [ProductDetails] drop constraint FK_prodDet_ProductCategoryId_prodCat_Id
 ALTER TABLE [ProductDetails]
 DROP COLUMN [ProductCategoryId]
--- Store.Model/DB/Scripts/3_Update.sql
+++ Store.Model/DB/Scripts/3_Update.sql
@@ -1,9 +1,10 @@
 ﻿USE StoreDB
 
-ALTER TABLE [Link_ConcreteProduct_Order] drop constraint FK__Link_Conc__Produ__2C3393D0
+ALTER TABLE [Link_ConcreteProduct_Order] drop constraint FK_link_ProdId_concrProd_Id
 ALTER TABLE [Link_ConcreteProduct_Order] DROP COLUMN [ProductId]
 
 ALTER TABLE [Link_ConcreteProduct_Order]
-ADD [ProductId] INT NOT NULL FOREIGN KEY REFERENCES [Product]([id])
+ADD [ProductId] INT NOT NULL,
+constraint FK_link_ProdId_prod_Id FOREIGN KEY (ProductId) REFERENCES [Product]([id])
 
 DROP TABLE [ConcreteProduct]
