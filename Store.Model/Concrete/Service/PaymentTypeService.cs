8b0215b727fdbc40fca8a9ec05ffe12ed08a5e00
Andrey Palamarchuk <studypalamarchuk@ukr.net>
2016-05-28 17:56:04 +0300

    Довідник виду оплат

--- Store.Model/Concrete/Service/PaymentTypeService.cs
+++ Store.Model/Concrete/Service/PaymentTypeService.cs
@@ -1,12 +1,42 @@
-﻿using Data.Repository.Concrete;
+﻿using System;
+using Data.Repository.Concrete;
 using Store.Model.Abstract;
 using Store.Model.Abstract.Service;
 using Store.Model.Entities.dbml;
+using System.Linq;
 
 namespace Store.Model.Concrete.Service
 {
     public class PaymentTypeService : BaseService<PaymentType>, IPaymentTypeService
     {
         public PaymentTypeService(IStoreRepository repository) : base(repository) { }
+
+        public void DeleteRecord(int id)
+        {
+            repository.BeginTransaction();
+            var model = GetById(id);
+            Delete(model);
+            repository.Commit();
+        }
+
+        public void UpsertRecord(int? id, string name)
+        {
+            repository.BeginTransaction();
+
+            if (id.HasValue && id != 0)
+            {
+                var model = GetById(id.Value);
+                model.Name = name;
+                repository.SubmitChange();
+            }
+            else
+            {
+                PaymentType model = new PaymentType() { Id = repository.Table<PaymentType>().Select(x => x.Id).Max() + 1, Name = name };
+                repository.Insert(model);
+            }
+
+            repository.Commit();
+        }
+        
     }
 }
--- Store.Web/Controllers/PaymentTypeController.cs
+++ Store.Web/Controllers/PaymentTypeController.cs
@@ -1,5 +1,11 @@
 ﻿using Store.Model.Abstract.Service;
+using Store.Model.Concrete.Service;
 using Store.Model.Entities.dbml;
+using Store.Web.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic;
 using System.Web.Mvc;
 
 namespace Store.Web.Controllers
@@ -15,10 +21,68 @@ namespace Store.Web.Controllers
             return View();
         }
 
-        public JsonResult GetList()
+        public JsonResult GetList(JQGridPostData jsonHeader)
         {
-            var result = service.GetAll();
-            return Json(result, JsonRequestBehavior.AllowGet);
+            IQueryable<PaymentType> data = service.GetAll();
+
+            int totalRecords = data.Count();
+            int totalPages = (int)Math.Ceiling(totalRecords / (double)jsonHeader.Rows);
+            jsonHeader.SetCorrectPage(totalRecords);
+            IQueryable<PaymentType> query = data
+                .OrderBy(jsonHeader.Sidx + " " + jsonHeader.Sord)
+                .Skip((jsonHeader.Page - 1) * jsonHeader.Rows).Take(jsonHeader.Rows);
+
+            var result = (from item in query
+                          select new
+                          {
+                              i = item.Id,
+                              cell = new IComparable[]
+                                        {
+                                            item.Id,
+                                            item.Name,
+                                            null
+                                        }
+                          }).ToArray();
+
+            var jsondata = new
+            {
+                total = totalPages,
+                page = jsonHeader.Page,
+                records = totalRecords,
+                rows = result
+            };
+
+            return Json(jsondata, JsonRequestBehavior.AllowGet);
         }
+
+        #region AddEdit
+        [HttpGet]
+        public ViewResult AddEdit(int? id)
+        {
+            PaymentType model;
+            if (!id.HasValue)
+                model = new PaymentType();
+            else
+                model = service.GetById(id.Value);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddEdit(int? id, string name)
+        {
+            ((PaymentTypeService)service).UpsertRecord(id, name);
+            return Content("Good");
+        }
+        #endregion
+
+        #region Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id.HasValue)
+                ((PaymentTypeService)service).DeleteRecord(id.Value);
+            return Content("Good");
+        }
+        #endregion
     }
 }
